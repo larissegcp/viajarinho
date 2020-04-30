32e8934bd435bc508377ffeabc1ff68fd38c7a48
Larisse Pinto <larissegpinto@gmail.com>
2020-04-30 18:21:37 -0300

    Testes de Navbar e banner

---
+++ js/componentes/navbar.js
@@ -0,0 +1,24 @@
+document.getElementById('navbarPrincipal').innerHTML = `
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Minha conta<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Meu carrinho</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Ok</button>
+    </form>
+  </div>
+</nav>
+
+`;
